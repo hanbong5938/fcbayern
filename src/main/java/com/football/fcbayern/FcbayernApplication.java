e2722494d3800d616b120d77a16eda85906b312f
uru1217 <uru1217@naver.com>
2020-01-05 19:06:18 +0900

    first commit

---
+++ src/main/java/com/football/fcbayern/FcbayernApplication.java
@@ -0,0 +1,13 @@
+package com.football.fcbayern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcbayernApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcbayernApplication.class, args);
+    }
+
+}
